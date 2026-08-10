de534aec084591bf564f4b72addf67220041d65c
issyone02 <olalere.isaiah@miva.edu.ng>
2026-08-10 22:01:30 +0100

    Deploy my profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/configss
+
 export default defineConfig({
  site: "https://issyone02.github.io",
  base: "/profile"
